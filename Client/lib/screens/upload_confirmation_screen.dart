9d9273d9e1b698d606c0aaef8f14b1b74b26af8d
DidulaFernando <didulafernando03@gmail.com>
2025-03-09 22:30:31 +0530

    Added : Ability to Upload Speech From File Picker - Initial Version

--- Client/lib/screens/upload_confirmation_screen.dart
+++ Client/lib/screens/upload_confirmation_screen.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:vocallabs_flutter_app/utils/constants.dart';
 import 'package:vocallabs_flutter_app/widgets/custom_button.dart';
-
-//to confirm the upload of the speech
+import 'package:file_picker/file_picker.dart';
 
 class UploadConfirmationScreen extends StatelessWidget {
   const UploadConfirmationScreen({super.key});
+
+  Future<void> _pickFile(BuildContext context) async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['mp3', 'wav', 'm4a'],
+    );
+
+    if (result != null) {
+      // Handle the selected file
+      PlatformFile file = result.files.single;
+      if (file.bytes != null) {
+        // Navigate to SpeechPlaybackScreen with the selected file bytes
+        Navigator.pushReplacementNamed(
+          context,
+          '/playback',
+          arguments: file.bytes,
+        );
+      }
+    } else {
+      // User canceled the picker
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,9 +53,7 @@ class UploadConfirmationScreen extends StatelessWidget {
             const SizedBox(height: 32),
             CustomButton(
               text: 'Confirm Upload',
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/feedback');
-              },
+              onPressed: () => _pickFile(context),
             ),
             const SizedBox(height: 16),
             CustomButton(
